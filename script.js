0ae0d02c277e8882bedf268b8f25bcd6b489ce39
fmagmych <mychaelmagnuson@gmail.com>
2023-04-10 19:54:24 -0500

    Adjusting the script to toggle a class for the body element when the mobile navigation is open to prevent scrolling

--- script.js
+++ script.js
@@ -1,36 +1,13 @@
-/* We can add some very basic JS programming here but it does not need to be
-anything crazy since where we are in the course, we have not learned much of any JS. 
-To make the project just a little more fun and challenging lets attempt to create a 
-sticky header*/
-
-// When the user scrolls the page, execute myFunction
-window.onscroll = function () {
-  myFunction();
-};
-
-// Get the header
-let header = document.getElementById("myHeader");
-
-// Get the offset position of the navbar
-let sticky = header.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position.
-// Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
-
 // Making the mobile button navigation work
 const buttonNavigationElement = document.querySelector(".btn-mobile-nav");
 const headerElement = document.querySelector(".header");
 
 buttonNavigationElement.addEventListener("click", function () {
   headerElement.classList.toggle("nav-open");
+  document.body.classList.toggle("no-scroll");
 });
+
+/////////////////////////////////////////////////////////////////////////////////
 
 // Adding code for a functional form
 const form = document.querySelector(".form");
